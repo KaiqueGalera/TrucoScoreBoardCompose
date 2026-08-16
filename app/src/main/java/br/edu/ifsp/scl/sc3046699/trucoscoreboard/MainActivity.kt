b0d5511e89775e89ae86a90042276d56e0f52e64
Kaique Galera <kaiquegalera@gmail.com>
2026-08-16 18:59:30 -0300

    feat: cria primeira coluna e mensagem para exibir placar

--- app/src/main/java/br/edu/ifsp/scl/sc3046699/trucoscoreboard/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3046699/trucoscoreboard/MainActivity.kt
@@ -4,9 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -14,6 +19,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import br.edu.ifsp.scl.sc3046699.trucoscoreboard.ui.theme.TrucoScoreBoardTheme
 import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -42,5 +51,18 @@ fun PlacarTruco(modifier: Modifier = Modifier) {
         pontosA == 11 -> "Equipe A entrou na mão de 11!"
         pontosB == 11 -> "Equipe B entrou na mão de 11!"
         else -> ""
+    }
+
+    Column(
+        modifier = modifier
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+
+        Text(
+            text = "Placar de Truco",
+            fontSize = 28.sp
+        )
     }
 }

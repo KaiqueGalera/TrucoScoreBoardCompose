452f65b6c96b9cdf30dac067883921e3037e695a
Kaique Galera <kaiquegalera@gmail.com>
2026-08-19 01:25:11 -0300

    feat: melhorias, testando propriedades das funções, melhorando o projeto no geral

--- app/src/main/java/br/edu/ifsp/scl/sc3046699/trucoscoreboard/MainActivity.kt
+++ app/src/main/java/br/edu/ifsp/scl/sc3046699/trucoscoreboard/MainActivity.kt
@@ -1,18 +1,22 @@
 package br.edu.ifsp.scl.sc3046699.trucoscoreboard
 
+import android.R
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -23,6 +27,12 @@ import androidx.compose.ui.Modifier
 import br.edu.ifsp.scl.sc3046699.trucoscoreboard.ui.theme.TrucoScoreBoardTheme
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Shadow
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
@@ -36,6 +46,7 @@ class MainActivity : ComponentActivity() {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     PlacarTruco(
                         modifier = Modifier.padding(innerPadding)
+                            .background(Color(0xff121826))
                     )
                 }
             }
@@ -65,104 +76,140 @@ fun PlacarTruco(modifier: Modifier = Modifier) {
         modifier = modifier
             .fillMaxSize(),
         horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.Center
+        verticalArrangement = Arrangement.SpaceBetween
     ) {
 
         Text(
             text = "Placar de Truco",
-            fontSize = 28.sp
+            fontSize = 41.sp,
+            modifier = Modifier.padding(top = 40.dp),
+            color = Color(0xFFF8FAFC)
         )
-
-        Spacer(modifier = Modifier.height(30.dp))
-
-        Text(
-            text = "Equipe A",
-            fontSize = 24.sp
-        )
-
-        Text(
-            text = pontosA.toString(),
-            fontSize = 48.sp
-        )
-
-        Row {
-            Button(
-                onClick = {
-                    if (!jogoFinalizado) {
-                        pontosA ++
-                    }
-                }
+        Spacer(modifier = Modifier.height(10.dp))
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(15.dp)
             ) {
-                Text("+1")
-            }
+            Text(
+                text = "Equipe A",
+                fontSize = 35.sp,
+                color = Color(0xFFF8FAFC)
+            )
 
-            Spacer(modifier = Modifier.width(10.dp))
+            Text(
+                text = pontosA.toString(),
+                fontSize = 45.sp,
+                color = Color(0xFFF8FAFC)
+            )
 
-            Button(
-                onClick = {
-                    if (!jogoFinalizado && !maoDeOnzeA) {
-                        pontosA += 3
-                    }
-                }
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(30.dp)
             ) {
-                Text("+3")
-            }
-        }
-
-        Spacer(modifier = Modifier.height(30.dp))
-
-        Text(
-            text = "Equipe B",
-            fontSize = 24.sp
-        )
-
-        Text(
-            text = pontosB.toString(),
-            fontSize = 48.sp
-        )
-
-        Row {
-            Button(
-                onClick = {
-                    if (!jogoFinalizado) {
-                        pontosB ++
-                    }
+                Button(
+                    onClick = {
+                        if (!jogoFinalizado) {
+                            pontosA ++
+                        }
+                    },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(0xff2563EB)
+                    )
+                ) {
+                    Text("+1",
+                        fontSize = 20.sp)
                 }
-            ) {
-                Text("+1")
-            }
 
-            Spacer(modifier = Modifier.width(10.dp))
-
-            Button(
-                onClick = {
-                    if (!jogoFinalizado && !maoDeOnzeB) {
-                        pontosB += 3
-                    }
+                Button(
+                    onClick = {
+                        if (!jogoFinalizado && !maoDeOnzeA) {
+                            pontosA += 3
+                        }
+                    },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(0xff2563EB)
+                    )
+                ) {
+                    Text("+3",
+                        fontSize = 20.sp)
                 }
-            ) {
-                Text("+3")
             }
         }
 
-        Spacer(modifier = Modifier.height(30.dp))
+        Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(15.dp)
+        ) {
+            Text(
+                text = "Equipe B",
+                fontSize = 35.sp,
+                color = Color(0xFFF8FAFC)
+            )
+
+            Text(
+                text = pontosB.toString(),
+                fontSize = 45.sp,
+                color = Color(0xFFF8FAFC)
+            )
+
+            Row(
+                horizontalArrangement = Arrangement.spacedBy(30.dp)
+            ) {
+                Button(
+                    onClick = {
+                        if (!jogoFinalizado) {
+                            pontosB ++
+                        }
+                    },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(0xff2563EB)
+                    )
+                ) {
+                    Text("+1",
+                        fontSize = 20.sp)
+                }
+                Button(
+                    onClick = {
+                        if (!jogoFinalizado && !maoDeOnzeB) {
+                            pontosB += 3
+                        }
+                    },
+                    colors = ButtonDefaults.buttonColors(
+                        containerColor = Color(0xff2563EB)
+                    )
+                ) {
+                    Text(
+                        text = "+3",
+                        fontSize = 20.sp)
+                }
+            }
+        }
 
         if (mensagem.isNotEmpty()) {
             Text(
                 text = mensagem,
-                fontSize = 20.sp
+                fontSize = 20.sp,
+                color = Color(0xff2563EB)
             )
         }
 
-        Spacer(modifier = Modifier.height(20.dp))
-
-        Button(
-            onClick = {
-                pontosA = 0
-                pontosB = 0
-            }
+        Column(
+            modifier = Modifier.fillMaxWidth(),
+            horizontalAlignment = Alignment.End
         ) {
-            Text("Reiniciar")
+            Button(
+                onClick = {
+                    pontosA = 0
+                    pontosB = 0
+                },
+                modifier = Modifier.padding(30.dp),
+                colors = ButtonDefaults.buttonColors(
+                    containerColor = Color(0xFFeab34d)
+                )
+            ) {
+                Text("Reiniciar",
+                    fontSize = 20.sp,
+                    color = Color(0xff0F172A))
+            }
         }
     }
 }
@@ -174,5 +221,3 @@ fun PlacarTrucoPreview() {
         PlacarTruco()
     }
 }
-
-
